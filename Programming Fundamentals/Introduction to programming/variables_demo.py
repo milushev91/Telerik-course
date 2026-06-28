f024ced2b16f604f25badc0f62e031d7af451c7b
milushev91 <mmilushev91@gmail.com>
2026-06-28 14:35:28 +0300

    Create variables_demo.py

---
+++ Programming Fundamentals/Introduction to programming/variables_demo.py
@@ -0,0 +1,9 @@
+first_name = "Pesho"
+last_name = "Georgiev"
+person_age = 31
+
+print(first_name + " " + last_name) # Pesho Georgiev
+print(person_age) # 31
+
+person_age = person_age + 1 
+print(person_age) # 32
