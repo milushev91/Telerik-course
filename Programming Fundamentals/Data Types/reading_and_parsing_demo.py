48daa4017b9e0f8b625c94c5f08e61b155b9f370
milushev91 <mmilushev91@gmail.com>
2026-06-28 15:49:17 +0300

    Create reading_and_parsing_demo.py

---
+++ Programming Fundamentals/Data Types/reading_and_parsing_demo.py
@@ -0,0 +1,12 @@
+# read three numbers
+# print their sum
+# print the max number
+
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+
+sum_numbers = first_number + second_number + third_number
+print(sum_numbers)
+
+print(max(first_number, second_number, third_number))
