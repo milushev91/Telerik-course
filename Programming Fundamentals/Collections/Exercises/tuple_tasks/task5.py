e5f3e15694dbca607dd279ed090fbf3954b16063
milushev91 <mmilushev91@gmail.com>
2026-07-06 13:31:41 +0300

    Create task5.py

---
+++ Programming Fundamentals/Collections/Exercises/tuple_tasks/task5.py
@@ -0,0 +1,17 @@
+# Task 5
+# Count the number of times a 
+# value (console input) is in a tuple.
+
+# Example:
+
+# 1, 2, 3, 4, 1
+# input 1
+# output 2
+# input 5
+# output 0
+
+new_tuple = (1, 2, 3, 4, 1)
+
+number = int(input())
+
+print(new_tuple.count(number))
