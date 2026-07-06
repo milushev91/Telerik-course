f7a0ac224a8c2c3b6d49ccdc73b34a6f12173685
milushev91 <mmilushev91@gmail.com>
2026-07-06 12:26:35 +0300

    Create task2.py

---
+++ Programming Fundamentals/Collections/Exercises/Dictionaries Tasks/task2.py
@@ -0,0 +1,15 @@
+# Task 2
+# Print the value for a key (console input) of a dictionary.
+
+# Example:
+
+# name -> Alice, age -> 29, city -> London
+# input name -> output Alice
+
+persons_info = {
+    "name": "Alice",
+    "age": 29,
+    "city": "London",
+}
+
+print(persons_info["name"])
