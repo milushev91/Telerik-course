b2d4bfd3dba75c33e95a97f4d2bd385d2eb69bfb
milushev91 <mmilushev91@gmail.com>
2026-07-06 13:26:21 +0300

    Create task7.py

---
+++ Programming Fundamentals/Collections/Exercises/List Tasks/task7.py
@@ -0,0 +1,17 @@
+# Task 7
+# Print the index of an element
+#  (console input) from a list.
+
+# Example:
+
+# a, b, c, d
+# input a
+# output 0
+
+
+letters = ["a", "b", "c", "d"]
+letter = input()
+
+letter_index = letters.index(letter)
+
+print(letter_index)
