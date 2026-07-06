9c5a00406a730ae1ddee7e68af56e2fa7ca6f0cf
milushev91 <mmilushev91@gmail.com>
2026-07-06 13:53:51 +0300

    Create task5.py

---
+++ Programming Fundamentals/Collections/Exercises/Set tasks/task5.py
@@ -0,0 +1,12 @@
+# Task 5
+# Remove an element (console input) of
+#  a set and print the set.
+
+# Example:
+
+# a, b, c, d
+# input a
+# output b, c, d
+
+my_set = {"a", "b", "c", "d"}
+item = input()
