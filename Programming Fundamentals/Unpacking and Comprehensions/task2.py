9ba4eec2f1c8b480739189cf9c54a3d327828911
milushev91 <mmilushev91@gmail.com>
2026-07-10 12:04:47 +0300

    Create task2.py

---
+++ Programming Fundamentals/Unpacking and Comprehensions/task2.py
@@ -0,0 +1,10 @@
+# Task 2
+# Declare three variables on a single line from
+# a single line console input with three values.
+# Example:
+
+# input Pesho Sofia Bulgaria
+# output Hi, my name is Pesho and I am from Sofia, Bulgaria
+
+name, city, country = input().split()
+print(f"Hi, my name is {name} and I am from {city}, {country}")
