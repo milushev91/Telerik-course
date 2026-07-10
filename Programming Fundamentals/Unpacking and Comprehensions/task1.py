8ddf827e68dc8f1a6691ad53ef21203c184ef744
milushev91 <mmilushev91@gmail.com>
2026-07-10 11:49:19 +0300

    Create task1.py

---
+++ Programming Fundamentals/Unpacking and Comprehensions/task1.py
@@ -0,0 +1,13 @@
+# Task 1
+# Declare two variables on a single line. 
+# Swap variable values on a single line.
+
+# Example:
+
+# a = 1
+# b = 2
+# output a is 2, b is 1
+
+a, b = 2, 1
+
+print(a, b)
