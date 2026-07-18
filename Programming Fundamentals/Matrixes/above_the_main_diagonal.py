d5fe1060306a4dd2918acbcc730a8c7ee8162131
milushev91 <mmilushev91@gmail.com>
2026-07-18 17:04:57 +0300

    Create above_the_main_diagonal.py

---
+++ Programming Fundamentals/Matrixes/above_the_main_diagonal.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+matrix = []
+sum_above_main_diagonal = 0
+for row in range(n):
+    row_numbers = [2 ** power for power in range(row, n + row)]
+    sum_above_main_diagonal += sum(row_numbers[row + 1::]) + row_numbers[row]
+
+print(sum_above_main_diagonal)
