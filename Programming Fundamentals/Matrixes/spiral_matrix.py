990cbc2f80f058958d4ea8a8c9ad7113d744d148
milushev91 <mmilushev91@gmail.com>
2026-07-21 14:46:44 +0300

    Create spiral_matrix.py

---
+++ Programming Fundamentals/Matrixes/spiral_matrix.py
@@ -0,0 +1,38 @@
+n = int(input())
+
+matrix = [[0] * n for _ in range(n)]
+
+num = 1
+top, bottom = 0, n - 1
+left, right = 0, n - 1
+
+while top <= bottom and left <= right:
+    # Fill top row
+    for col in range(left, right + 1):
+        matrix[top][col] = num
+        num += 1
+    top += 1
+
+    # Fill right column
+    for row in range(top, bottom + 1):
+        matrix[row][right] = num
+        num += 1
+    right -= 1
+
+    # Fill bottom row
+    if top <= bottom:
+        for col in range(right, left - 1, -1):
+            matrix[bottom][col] = num
+            num += 1
+        bottom -= 1
+
+    # Fill left column
+    if left <= right:
+        for row in range(bottom, top - 1, -1):
+            matrix[row][left] = num
+            num += 1
+        left += 1
+
+# Print the matrix
+for row in matrix:
+    print(*row)
