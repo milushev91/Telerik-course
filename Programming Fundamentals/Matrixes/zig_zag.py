ffd7a26734e870f37dfdb4dbbb66efe346f021ff
milushev91 <mmilushev91@gmail.com>
2026-07-21 14:26:54 +0300

    Update zig_zag.py

--- Programming Fundamentals/Matrixes/zig_zag.py
+++ Programming Fundamentals/Matrixes/zig_zag.py
@@ -1,8 +1,8 @@
 rows, cols = [int(num) for num in input().split()]
-print(rows, cols)
 
 matrix = []
 row_start = 1 
+
 for i in range(rows):
     col_start = row_start
     row = []
@@ -14,19 +14,23 @@ for i in range(rows):
     matrix.append(row)
     row_start += 3
 
-for row in range(rows):
+sum_score = matrix[0][0]
+
+for row in range(rows - 1):
 
     if row % 2 == 0:
         for col in range(cols - 1):
             if col % 2 == 0:
-                print(matrix[row + 1][col + 1])
+                sum_score += matrix[row + 1][col + 1]
             else:
-                print(matrix[row][col + 1])
+                
+                sum_score += matrix[row][col + 1]
     else:
         for col in range(cols - 1, 0, -1):
             if col % 2 != 0:
-                print(mat)
+                sum_score += matrix[row + 1][col - 1]
+                
+            else:
+                sum_score += matrix[row][col - 1]
 
-    break
-print(*matrix, sep="\n")
-
+print(sum_score)
