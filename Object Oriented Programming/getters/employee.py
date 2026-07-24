6937dcd9956c9282a83cb5aca984cd2851dd8c1f
milushev91 <mmilushev91@gmail.com>
2026-07-24 07:50:47 +0300

    Update employee.py

--- Object Oriented Programming/getters/employee.py
+++ Object Oriented Programming/getters/employee.py
@@ -1,11 +1,22 @@
 class Employee:
-    def __init__(self, full_name, salary, department):
+    def __init__(self, full_name, department, salary):
         self.full_name = full_name
-        self.salary = salary
         self.department = department
+        self.salary = salary
+
+    @property 
+    def salary(self):
+        return self._salary
+        
+    @salary.setter
+    def salary(self, value):
+        if value < 0:
+            raise ValueError("Employee's salary must be a postive number")
+            
+        self._salary = value
     
     def public_info(self):
         return f"{self.full_name}, Department: {self.department}"
     
     def internal_info(self):
-        return f"{self.publi_info()}, Salary: {self.salary}"
+        return f"{self.public_info()}, Salary: {self.salary}"
