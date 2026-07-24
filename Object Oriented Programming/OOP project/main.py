3e86ae053e700d3d9a6847b24f7795b3b28064dc
milushev91 <mmilushev91@gmail.com>
2026-07-24 16:33:27 +0300

    Update main.py

--- Object Oriented Programming/OOP project/main.py
+++ Object Oriented Programming/OOP project/main.py
@@ -1,13 +1,11 @@
 from item_status import ItemStatus
 from board_item import BoardItem
-from datetime import date, timedelta
- 
-def add_days_to_now(d):
-    """This function help us parse the number of days(d) which is an integer into a datetime object.
-    """
-    return date.today() + timedelta(days = d)
+from calculate_date import add_days_to_now
 
-item = BoardItem('Registration doesn\'t work', add_days_to_now(2))
+
+item = BoardItem('2323232323', add_days_to_now(2))
 print(item.title)
 print(item.due_date)
 print(item.status)
+
+
